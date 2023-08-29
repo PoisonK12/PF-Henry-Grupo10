47d86be47dc19aceb85862e32672f659430bb2cc
GGuenov <german.guenov@gmail.com>
2023-08-29 15:31:26 +0100

    cualquier

---
+++ proyecto-sin-nombre-definido/Server/src/models/holdings.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "holding",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      availability: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
