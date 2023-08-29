8e581857f085e73cc37ca5741d7e338866d144e0
JustinaMihura <justinamihura@hotmail.com>
2023-08-29 14:33:37 -0300

    hola

--- proyecto-sin-nombre-definido/Client/src/components/Nav/Nav.jsx
+++ proyecto-sin-nombre-definido/Client/src/components/Nav/Nav.jsx
@@ -8,11 +8,17 @@ export const Nav = () => {
     
     return (
         <div className={style.nav}>
-            <div>  
-                <Link to="/home"><img className="img" src={logo}></img></Link>
-            </div>
-          
+            <Link to="/home"><img className="img" src={logo}></img></Link>
+            <h1> Work in Progress </h1>
             <Link to="/home"><button className="button_home"> Home </button></Link>
+            <ul>  Menú
+                <Link to="admin"><li> Admin </li></Link>
+                <Link to="profile"><li> Perfil</li></Link>
+                <Link to="own_property"><li> Mis propiedades</li></Link>
+                <Link to="orders"><li> Ordenes pendientes </li></Link>
+                <li>Cerrar sesión</li>
+            </ul>
+
         </div>
     )
 };
