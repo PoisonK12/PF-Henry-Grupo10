3776e61fbb72ee521ab669882b1b8c6528081113
PoisonK12 <sadriancampos.10@gmail.com>
2023-08-29 15:29:39 -0400

    Correcion relacion modelos

--- proyecto-sin-nombre-definido/Server/src/db.js
+++ proyecto-sin-nombre-definido/Server/src/db.js
@@ -13,7 +13,7 @@ UserModel(sequelize);
 RentModel(sequelize);
 AssetModel(sequelize);
 
-const { User, Rent, Asset } = sequelize.models
+const { User, Rent, Asset, Amenity } = sequelize.models
 
 User.hasMany(Asset, { through: 'userAssets' })
 Asset.belongsTo(User, { through: 'userAssets' })
@@ -21,6 +21,7 @@ Asset.belongsTo(User, { through: 'userAssets' })
 User.hasMany(Rent, { through: 'userRents' })
 Rent.belongsTo(User, { through: 'userRents' })
 
-Asset.hasMany(Amenitie.{through:"AssetAmenities"})
-Amenitie.hasMany(Asset.{through:"AssetAmenities"})
+Asset.hasMany(Amenity, { through: 'assetAmenities' })
+Amenity.hasMany(Asset, { through: 'assetAmenities' })
+
 module.exports = { sequelize }
