46d554bf3afae6ca217327a58e7138a63f545546
tranceblastr <brian.andrais@gmail.com>
2023-08-29 16:33:39 -0300

    UserHandler

--- proyecto-sin-nombre-definido/Server/src/handlers/userHandler.js
+++ proyecto-sin-nombre-definido/Server/src/handlers/userHandler.js
@@ -36,9 +36,7 @@ const userPostHandler = async (req, res) => {
     landlord,
     favorites,
     history)
-    
     res.status(200).json("Usuario creado con exito!")
-
    } catch (error) {
     console.log(error)
     res.status(404).json("Error en la creacion del usuario!")
@@ -49,16 +47,56 @@ const getUserByIdHandler = async (req, res) => {
   const { id } = req.params
   
   try {
-    const user = await getUserByIdController 
-
-    
+    const user = await getUserByIdController(id)
+    res.status(200).json(user)
   } catch (error) {
     console.log(error)
     res.status(404).json("Error encontrando el usuario!")  
   }
 }
 
-const userPutHandler = async (req, res) => {}
+const userPutHandler = async (req, res) => {
+  const {  
+    userName,
+    fullName,
+    profilePic,
+    birthDate,
+    phoneNumber,
+    verificationNumber,
+    gender,
+    address,
+    nationality,
+    email,
+    password,
+    review,
+    landlord,
+    favorites,
+    history } = req.body
+
+  try {
+    await userEditController(
+      userName,
+      fullName,
+      profilePic,
+      birthDate,
+      phoneNumber,
+      verificationNumber,
+      gender,
+      address,
+      nationality,
+      email,
+      password,
+      review,
+      landlord,
+      favorites,
+      history
+    )
+    res.status(200).json("Usuario editado con exito!")
+  } catch (error) {
+    console.log(error)
+    res.status(404).json("Error editando el usuario!")  
+  }
+}
 
 const userDeleteOrBanHandler = async (req, res) => {}
 
