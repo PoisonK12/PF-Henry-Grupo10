87a5833bbe6ff17c188fb3fd53aa7d2de84ac034
SoyNexoo <101372442+SoyNexoo@users.noreply.github.com>
2023-08-29 15:47:37 -0300

    Navbar

--- proyecto-sin-nombre-definido/Client/src/components/Nav/Nav.jsx
+++ proyecto-sin-nombre-definido/Client/src/components/Nav/Nav.jsx
@@ -1,30 +1,39 @@
-import React from "react";
-import {Link, NavLink} from "react-router-dom"
-import logo from "../../assets/logo.png"
-import style from "./Nav.module.css"
-
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import style from "./Nav.module.css";
 
 export const Nav = () => {
-    
-    return (
-        <nav className={style.nav}>
-            <Link to="/" className={style.logo}><img src={logo}></img></Link>
-            <div className={style.menu}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <ul>
-                <li>
-                    <NavLink to="/home">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/contacts">Contactos</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/properties">Propiedades</NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={style.nav}>
+      <Link to="/" className={style.logo}>
+        <img src={logo}></img>
+      </Link>
+      <div
+        className={style.menu}
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+          console.log("hola");
+          console.log(menuOpen)
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink to="/home">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contacts">Contactos</NavLink>
+        </li>
+        <li>
+          <NavLink to="/properties">Propiedades</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
