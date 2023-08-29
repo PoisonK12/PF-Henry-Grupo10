2d55ddf3b2dd2ce62fe4771fea5715479f2a7874
JustinaMihura <justinamihura@hotmail.com>
2023-08-29 14:47:07 -0300

    h

--- proyecto-sin-nombre-definido/Client/src/App.jsx
+++ proyecto-sin-nombre-definido/Client/src/App.jsx
@@ -7,7 +7,7 @@ import Landing from './views/Landing/Landing';
 import NotFound from './views/404/404';
 import Detail  from './views/Details/Detail';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Footer from "./components/Footer/Footer"
 
 function App() {
   const location = useLocation()
@@ -21,6 +21,7 @@ function App() {
       <Route path='/home' element={<Home/>}/>
       <Route path="*" element={<NotFound/>}/>
      </Routes>
+     {location.pathname !== "/" && <Footer/>}
     </>
   )
 }
