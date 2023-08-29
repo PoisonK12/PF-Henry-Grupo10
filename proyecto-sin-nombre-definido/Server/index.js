4d6f049e95a129123d2b7e59ea46ae0747961030
PoisonK12 <sadriancampos.10@gmail.com>
2023-08-29 14:56:25 -0400

    relacion modelos

--- proyecto-sin-nombre-definido/Server/index.js
+++ proyecto-sin-nombre-definido/Server/index.js
@@ -1,5 +1,7 @@
 const server = require('./src/server')
+const { sequelize } = require('./src/db')
 
 server.listen(3001, () => {
+  sequelize.sync()
   console.log('Server is running on port 3001');
 })
---
+++ proyecto-sin-nombre-definido/Server/src/db.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize')
+require("dotenv").config();
+
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dbname`)
+
+module.exports= {sequelize}
