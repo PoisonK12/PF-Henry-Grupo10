3d0dba2a363c1d15e1fdd6370903e9f78dbf2f09
PoisonK12 <sadriancampos.10@gmail.com>
2023-08-29 15:18:50 -0400

    Relacion de modelos

--- proyecto-sin-nombre-definido/Server/src/db.js
+++ proyecto-sin-nombre-definido/Server/src/db.js
@@ -1,8 +1,26 @@
 const { Sequelize } = require('sequelize')
 require("dotenv").config();
 
+const UserModel = require('./models/users')
+const RentModel = require('./models/rents')
+const AssetModel = require('./models/assets')
+
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dbname`)
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dbname`, { logging: false })
 
-module.exports= {sequelize}
+UserModel(sequelize);
+RentModel(sequelize);
+AssetModel(sequelize);
+
+const { User, Rent, Asset } = sequelize.models
+
+User.hasMany(Asset, {through:'userAssets'})
+Asset.belongsTo(User,{through:'userAssets'})
+
+User.hasMany(Rent, {through: 'userRents'})
+Rent.belongsTo(User, {through: 'userRents'})
+
+
+
+module.exports = { sequelize }
