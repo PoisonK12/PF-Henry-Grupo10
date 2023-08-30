34417287893856e92c434f9c6b47d2399bb5dc87
GGuenov <german.guenov@gmail.com>
2023-08-30 15:43:18 +0100

    conflicto de prettier

--- proyecto-sin-nombre-definido/Server/src/controllers/assetController.js
+++ proyecto-sin-nombre-definido/Server/src/controllers/assetController.js
@@ -1,37 +1,36 @@
-const { Asset, Amenity } = require('../db')
-const { Op } = require('sequelize')
+const { Asset, Amenity } = require("../db");
+const { Op } = require("sequelize");
 
 const getAllAssets = async () => {
-
   const pageAsNumber = Number.parseInt(req.query.page);
   const sizeAsNumber = Number.parseInt(req.query.size);
 
   let page = 0;
-  if(!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
-    page = pageAsNumber
+  if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
+    page = pageAsNumber;
   }
 
   let size = 10;
   if (!Number.isNaN(sizeAsNumber) && sizeAsNumber > 0 && sizeAsNumber < 10) {
-    size = sizeAsNumber
+    size = sizeAsNumber;
   }
 
   const assets = await Asset.findAndCountAll({
     limit: size,
-    offset: page * size
-  })
-  return assets
-}
+    offset: page * size,
+  });
+  return assets;
+};
 
 const getAssetById = async (id) => {
   const asset = await Asset.findOne({
     where: {
-      id: id
+      id: id,
     },
     include: {
       model: Amenity,
-      through: {attribute: []}
-    }
-  })
-  return asset
-}
+      through: { attribute: [] },
+    },
+  });
+  return asset;
+};
