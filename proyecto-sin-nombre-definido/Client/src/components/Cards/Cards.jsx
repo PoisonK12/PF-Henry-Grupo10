3b9a59cf4661fee643628901fed3c3db2e2d3631
OsKing <oscar00gaona@gmail.com>
2023-08-30 11:11:03 -0500

    feat card ajustes y home card detalada

--- proyecto-sin-nombre-definido/Client/src/components/Cards/Cards.jsx
+++ proyecto-sin-nombre-definido/Client/src/components/Cards/Cards.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+import utils from "./utils";
+import Card from "../Card/Card";
 
 const Cards = () => {
-    
-
-
+  console.log('Infoo',utils);
+  let info = utils;
   return (
-    <div>Cards</div>
-  )
-}
+    <div>
+        {
+          info.map((prop) => <Card name={prop.name} description={prop.description} address={prop.address} location={prop.location}
+          country={prop.country} images={prop.images[0]} onSale={prop.onSale} total={prop.total} perPerson={prop.perPerson} 
+          rooms={prop.rooms} bathrooms={prop.bathrooms} coveredArea={prop.coveredArea} totalArea={prop.totalArea}/>)
+        }
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
--- proyecto-sin-nombre-definido/Client/src/views/Home/Home.jsx
+++ proyecto-sin-nombre-definido/Client/src/views/Home/Home.jsx
@@ -5,6 +5,7 @@ import playa2 from '../../assets/images/image02.jpeg';
 import playa3 from '../../assets/images/image0.jpg';
 import style from './Home.module.css';
 import ButtonUp from '../../components/ButtonUp/ButtonUp';
+import Cards from '../../components/Cards/Cards';
 
 export const Home = () => {
  
@@ -44,68 +45,8 @@ export const Home = () => {
             </div>
           </section>
       </div>
-      <div className={style.card}>
-        <div className={`card mb-12`}>
-            <div className="row g-0">
-              <div className="col-md-4">
-                <img src={hotel} className="img-fluid rounded-start" alt="..." />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                  </p>
-                  <p className="card-text">
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                  </p>
-                </div>
-              </div>
-            </div>
-        </div>
-      </div>
-      <div className={style.card}>
-        <div className={`card mb-12`}>
-            <div className="row g-0">
-              <div className="col-md-4">
-                <img src={hotel} className="img-fluid rounded-start" alt="..." />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                  </p>
-                  <p className="card-text">
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                  </p>
-                </div>
-              </div>
-            </div>
-        </div>
-        
-      </div>
-      <div className={style.card}>
-        <div className={`card mb-12`}>
-            <div className="row g-0">
-              <div className="col-md-4">
-                <img src={hotel} className="img-fluid rounded-start" alt="..." />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                  </p>
-                  <p className="card-text">
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                  </p>
-                </div>
-              </div>
-            </div>
-        </div>
-      </div>
-        <ButtonUp/> 
+      <Cards/>
+      <ButtonUp/> 
       </div>
     </>
   );
