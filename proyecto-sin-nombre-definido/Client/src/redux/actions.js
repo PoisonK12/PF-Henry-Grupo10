803ab78417b6b7e5a15aea6ee356550180f2600e
SoyNexoo <101372442+SoyNexoo@users.noreply.github.com>
2023-08-31 14:22:52 -0300

    Cards feaet

--- proyecto-sin-nombre-definido/Client/src/redux/actions.js
+++ proyecto-sin-nombre-definido/Client/src/redux/actions.js
@@ -2,16 +2,16 @@ import axios from "axios";
 import { GET_ALL_PROPERTIES } from "./types";
 
 
-export const getAllProperties = () => {
+export const getAllProperties = (page) => {
     return async(dispatch) => {
         try {
-            const {data} = await axios("/assets?size=10&page=1")
+            const {data} = await axios(`/assets?size=10&page=${page}`)
             return dispatch({
                 type: GET_ALL_PROPERTIES,
                 payload: data
             })
         } catch (error) {
-            
+            console.error(error)
         }
     }
 }
