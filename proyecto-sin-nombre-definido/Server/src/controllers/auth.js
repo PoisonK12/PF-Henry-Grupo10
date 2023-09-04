f21a9cd20fcc165f2e2dbeed2df553d618ca57ab
PoisonK12 <sadriancampos.10@gmail.com>
2023-09-04 16:08:28 -0400

    login 2.0

--- proyecto-sin-nombre-definido/Server/src/controllers/auth.js
+++ proyecto-sin-nombre-definido/Server/src/controllers/auth.js
@@ -1,18 +1,43 @@
-const { encrypt, compare } = require('../handlers/handleBcrypt')
-const { User } = require('../db')
+const { encrypt, compare } = require('../handlers/handleBcrypt');
+const { User } = require('../db');
 
 const registerCtrl = async (req, res) => {
   try {
-    const { email, password, name } = req.body
-    const passwordHash = await encrypt(password)
-    const registerUser = await userModel.create({
+    const { email, password, name } = req.body;
+    const passwordHash = await encrypt(password);
+    const registerUser = await User.create({
       email,
       name,
-      password: passwordHash
-    })
+      password: passwordHash,
+    });
 
-    res.send({ data: registerUser })
+    res.send({ data: registerUser });
   } catch (error) {
-
+    res.status(500).json({ error: error.message }); 
   }
-}
+};
+
+const loginCtrl = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ msg: 'Invalid credentials' }); 
+    }
+    const checkedPassword = await compare(password, user.password);
+    if (checkedPassword) {
+      const tokenSession = await tokenSign(user); 
+      res.send({
+        data: user,
+        token: tokenSession,
+      });
+    } else {
+      res.status(503).json({ msg: 'Error' }); // 
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message }); 
+  }
+};
+
+
+module.exports = {registerCtrl, loginCtrl}
