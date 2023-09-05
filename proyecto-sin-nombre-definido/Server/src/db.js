205f72224c70c4f7fcd23236e930115825a982f2
tranceblastr <brian.andrais@gmail.com>
2023-09-05 16:02:10 -0300

    correccion validations y db

--- proyecto-sin-nombre-definido/Server/src/db.js
+++ proyecto-sin-nombre-definido/Server/src/db.js
@@ -7,9 +7,6 @@ const AssetModel = require("./models/assets");
 const ReviewModel = require("./models/reviews");
 
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-console.log(DB_HOST);
-console.log(DB_PASSWORD);
-console.log(DB_USER);
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/GOAT10`,
   { logging: false }
