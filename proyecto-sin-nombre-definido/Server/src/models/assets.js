4694fa6c171035fe7aeab022160aaeb110bd2b7b
PoisonK12 <sadriancampos.10@gmail.com>
2023-09-05 12:10:48 -0400

    Soft delete 3.0

--- proyecto-sin-nombre-definido/Server/src/models/assets.js
+++ proyecto-sin-nombre-definido/Server/src/models/assets.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const Asset = sequelize.define(
+  sequelize.define(
     "Asset",
     {
       id: {
@@ -23,15 +23,15 @@ module.exports = (sequelize) => {
       },
       location: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull:false,
       },
       country: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type :DataTypes.STRING,
+        allowNull: false
       },
       images: {
         type: DataTypes.ARRAY(DataTypes.TEXT),
-        allowNull: false,
+        allowNull: false
       },
       onSale: {
         type: DataTypes.BOOLEAN,
@@ -44,23 +44,23 @@ module.exports = (sequelize) => {
       },
       rooms: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: false
       },
       bathrooms: {
         type: DataTypes.INTEGER,
       },
       averageScore: {
-        type: DataTypes.ARRAY(DataTypes.DECIMAL),
-        defaultValue: [0.0, 0.0],
+        type: DataTypes.ARRAY(DataTypes.FLOAT),
+        defaultValue: [0,0]
       },
       coveredArea: {
-        type: DataTypes.FLOAT,
+        type: DataTypes.FLOAT
       },
       totalArea: {
-        type: DataTypes.FLOAT,
+        type: DataTypes.FLOAT
       },
       amenities: {
-        type: DataTypes.ARRAY(DataTypes.INTEGER),
+        type: DataTypes.ARRAY(DataTypes.INTEGER)
       },
       eliminado: { 
         type: DataTypes.BOOLEAN,
