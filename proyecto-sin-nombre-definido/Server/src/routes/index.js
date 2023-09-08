5c0e603d1e20be2b03c7b8420534cd4373764b3e
PoisonK12 <sadriancampos.10@gmail.com>
2023-09-08 15:33:23 -0400

    correccion menor

--- proyecto-sin-nombre-definido/Server/src/routes/index.js
+++ proyecto-sin-nombre-definido/Server/src/routes/index.js
@@ -5,11 +5,8 @@ const amenitiesRouter = require("./amenitiesRouter");
 const authRouter = require("./auth");
 const reviewsRouter = require("./reviewsRouter");
 const logOutRouter = require('./logOut')
-<<<<<<< HEAD
 const rentsRouter = require("./rentsRouters");
-=======
 const googleLoginRouter = require('./googleLogin')
->>>>>>> 6af67d0595d6cf2ef50fd0b36f5a17a639b70466
 const router = Router();
 
 router.use('/googleAuth', googleLoginRouter)
