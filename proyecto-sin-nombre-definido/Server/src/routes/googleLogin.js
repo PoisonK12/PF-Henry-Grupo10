16aa913ca3475d7f086a7510af18cd38d2789c30
PoisonK12 <sadriancampos.10@gmail.com>
2023-09-13 16:08:03 -0400

    google login para probar

--- proyecto-sin-nombre-definido/Server/src/routes/googleLogin.js
+++ proyecto-sin-nombre-definido/Server/src/routes/googleLogin.js
@@ -1,20 +1,29 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const passport = require('passport')
 
 
 const googleLoginRouter = Router()
 
 //Permite iniciar sesion
-googleLoginRouter.get('/auth/google',
+googleLoginRouter.get('/',
   passport.authenticate('google', { scope: ['profile', 'email'] })
 );
 
+googleLoginRouter.get('/success', (req, res) => {
+  res.json({
+    message: 'Te has logueado exitosamente',
+    name: req.user.displayName, 
+    email: req.user.emails[0].value, 
+    pic: req.user.photos[0].value
+  });
+});
+
+
 // Ruta de callback para Google después de la autenticación
-googleLoginRouter.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
+googleLoginRouter.get('/callback',
+  passport.authenticate('google', { failureRedirect: '/failed' }),
   (req, res) => {
-    // Redirecciona a la página de inicio o a donde quieras después de la autenticación exitosa
-    res.redirect('/');
+    res.redirect('/success');
   }
 );
 
