ac642259651f4e297287ed7c3fba39ba923b88c8
GGuenov <german.guenov@gmail.com>
2023-09-17 19:05:26 +0100

    Conflicto de cookie session solucionado

--- proyecto-sin-nombre-definido/Server/src/server.js
+++ proyecto-sin-nombre-definido/Server/src/server.js
@@ -1,31 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-require('./helpers/middlewares/passport-config');
-const router = require('./routes/index');
-const cookieSession = require('cookie-session');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+require("./helpers/middlewares/passport-config");
+const router = require("./routes/index");
+const cookieSession = require("cookie-session");
 
 const server = express();
 
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use(express.json());
 server.use(cors());
 
-server.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
+server.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Configura passport
 server.use(passport.initialize());
-server.use(cookieSession({
-  name: 'session',
-  keys: [process.env.COOKIE_KEY1, process.env.COOKIE_KEY2],
-}));
+
 server.use(passport.session());
 
 server.use(router);
