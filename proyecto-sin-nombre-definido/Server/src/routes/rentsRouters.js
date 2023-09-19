989b17f402eac4b69f5e07e0b69f7658217a9b18
GGuenov <german.guenov@gmail.com>
2023-09-19 14:19:58 +0100

    Booking Cancelation

--- proyecto-sin-nombre-definido/Server/src/routes/rentsRouters.js
+++ proyecto-sin-nombre-definido/Server/src/routes/rentsRouters.js
@@ -1,14 +1,16 @@
 const { Router } = require("express");
 const {
   // deleteRentByIdHandler,
-  createRentHandler,
   createBookHandler,
+  deleteBookingHandler,
+  createRentHandler,
 } = require("../handlers/rentHandler");
 
 const rentsRouter = Router();
 
 rentsRouter.post("/create/:id", createRentHandler);
 rentsRouter.post("/reserva", createBookHandler);
+rentsRouter.delete("/reserva/:id", deleteBookingHandler);
 
 //rentsRouter.delete("/:id", deleteRentByIdHandler);
 
