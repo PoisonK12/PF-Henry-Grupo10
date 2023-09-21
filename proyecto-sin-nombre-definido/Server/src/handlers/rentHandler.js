5aa57c04b03994ec7e1b8fd0ab240797cf25b0fc
OsKing <oscar00gaona@gmail.com>
2023-09-21 02:06:16 -0500

    Email de rentas

--- proyecto-sin-nombre-definido/Server/src/handlers/rentHandler.js
+++ proyecto-sin-nombre-definido/Server/src/handlers/rentHandler.js
@@ -1,4 +1,8 @@
 const { type } = require("os");
+const nodemailer = require("nodemailer");
+const path = require('path');
+const fs = require('fs'); // Asegúrate de requerir fs
+const templatePath = path.join(__dirname, 'mailingTemplates', 'Rentas.html');
 
 // const { rentSchemePost } = require("../helpers/validations/rentValidation.js");
 const {
@@ -9,6 +13,53 @@ const {
   getRentById,
 } = require("../controllers/rentController.js");
 // const { final } = require("../controllers/rentController.js");
+const finalHandler = async (req, res) => {
+  const id = req.params;
+  try {
+    const response = await final(id);
+    fs.readFile(templatePath, 'utf8', async (err, html) => {
+      if (err) {
+        console.error('Error al leer el archivo de plantilla:', err);
+        return res.status(500).json({ error: "Error al registrar el usuario", details: err.message });
+      }
+    var transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465, // Cambiado de "post" a "port"
+      secure: true,
+      auth: {
+        user: "greattravel.contact@gmail.com",
+        pass: "hbacczxxirmcjmht"
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+    });
+
+     // Detalles del correo
+     var mailOptions = {
+      from: 'greattravel.contact@gmail.com',
+      to: "oscar00gaona@gmail.com", // Utiliza la dirección del destinatario proporcionada en el cuerpo de la solicitud
+      subject:"Gracias por seleccionarnos",
+      html: html, // Puedes cambiar esto a HTML si lo deseas
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        res.status(500).send(error.message);
+      } else {
+        console.log("Email Enviado")
+        res.status(200).jsonp(req.body);
+      }
+    });
+
+    res.status(200).json(response);
+  })
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: error.message });
+  }
+};
+
 
 const createBookHandler = async (req, res) => {
   const { assetId, userId, checkInDate, checkOutDate } = req.body;
@@ -84,6 +135,7 @@ const getRentByIdHandler = async (req, res) => {
   try {
     const response = await getRentById(id);
 
+
     res.status(200).json(response);
   } catch (error) {
     console.log(error);
@@ -91,16 +143,7 @@ const getRentByIdHandler = async (req, res) => {
   }
 };
 
-const finalHandler = async (req, res) => {
-  const id = req.params;
-  try {
-    const response = await final(id);
-    res.status(200).json(response);
-  } catch (error) {
-    console.log(error);
-    res.status(404).json({ error: error.message });
-  }
-};
+
 
 module.exports = {
   createBookHandler,
